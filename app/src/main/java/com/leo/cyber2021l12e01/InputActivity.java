831f827ab2946a9184c8bbeae625bab73f15ef5b
Kfir Awad <7997354+Leo40Git@users.noreply.github.com>
2020-02-16 10:21:26 +0200

    Initial commit

---
+++ app/src/main/java/com/leo/cyber2021l12e01/InputActivity.java
@@ -0,0 +1,14 @@
+package com.leo.cyber2021l12e01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_input);
+	}
+}
